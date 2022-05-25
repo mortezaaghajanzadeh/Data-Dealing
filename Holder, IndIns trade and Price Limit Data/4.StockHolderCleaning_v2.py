3d694833bdaab0316618acf1b430dafd789ebdb9
Seyyed Morteza Aghajanzadeh <mortezaaghajanzadeh@gmail.com>
2022-05-25 13:22:45 +0430

    Update 4.StockHolderCleaning_v2.py

--- Holder, IndIns trade and Price Limit Data/4.StockHolderCleaning_v2.py
+++ Holder, IndIns trade and Price Limit Data/4.StockHolderCleaning_v2.py
@@ -1,4 +1,5 @@
 #%%
+from unittest import result
 import pandas as pd
 import re
 import numpy as np
@@ -113,6 +114,17 @@ df1 = df1[~df1.Holder.isnull()]
 df1 = df1[df1.Holder != "nan"]
 print(len(df1))
 #%%
+gdata = pdf[["group_name", "name"]].drop_duplicates().dropna()
+mapingdict = dict(zip(gdata.name, gdata.group_name))
+df1["group_name"] = df1["name"].map(mapingdict)
+gdata = pdf[["group_id", "group_name"]].dropna().drop_duplicates()
+mapingdict = dict(zip(gdata.group_name, gdata.group_id))
+df1["group_id"] = df1["group_name"].map(mapingdict)
+df1 = df1.dropna()
+df1[(df1.name == "کماسه") & (df1.date >= 20170325)].sort_values(by="date").head()
+
+
+#%%
 df2 = df1[["Holder_id", "Holder", "date"]]
 df2 = df2.sort_values(by=["Holder_id"])
 Holders = df2.drop_duplicates(
@@ -174,7 +186,266 @@ df1.loc[df1.Holder_id2.isnull(), "Holder_id2"] = df1.loc[
 df1["Holder_id"] = df1.Holder_id2
 df1 = df1.drop(columns=["Holder_id2"])
 #%%
-df1[df1.Holder == "اخابر"].drop_duplicates(subset=["Holder_id"])[
+df1[df1.Holder == "شستا"].drop_duplicates(subset=["Holder_id"])[
     ["Holder_id", "name", "date", "Holder", "type", "Number", "Total"]
 ]
+#%%
+df1 = (
+    df1.groupby(
+        [
+            "stock_id",
+            "name",
+            "Firm",
+            "date",
+            "group_name",
+            "group_id",
+            "jalaliDate",
+            "Holder_id",
+            "Holder",
+            "Total",
+            "close_price",
+            "type",
+        ]
+    )
+    .agg({"Number": sum, "Percent": sum})
+    .reset_index()
+)
+#%%
+df1[(df1.name == "کماسه") & (df1.date >= 20170325)].sort_values(by="date").head()
+
+# %%
+df1 = df1[
+    [
+        "date",
+        "name",
+        "stock_id",
+        "jalaliDate",
+        "group_name",
+        "group_id",
+        "Holder",
+        "Holder_id",
+        "Number",
+        "type",
+        "Percent",
+        "Total",
+        "close_price",
+    ]
+]
+df1.head()
+df1[(df1.name == "آ س پ") & (df1.date == 20140511.0)]
+#%%
+a = df1.groupby("date").size().to_frame().reset_index()
+a.plot(y=0, use_index=True)
+# %%
+def sumPercent(df):
+    gg = df.groupby(["date", "name"])
+    return gg.Percent.sum()
+
+
+a = sumPercent(df1)
+a[a > 100]
+
+#%%
+df1[df1.name == "کفرا"][df1.Holder_id == 99.0][df1.date >= 20081222][
+    ["name", "date", "jalaliDate", "Number", "Percent", "Total", "close_price"]
+].head(50)
+#%%
+
+def Cleaning(gg):  # , g_keys):
+    # i = g.name
+
+    ## Filling the Gaps
+    if len(gg) > 3:
+        v1 = gg["stock_id"][~gg["stock_id"].isna()].index[-1]
+        v2 = gg["stock_id"][~gg["stock_id"].isna()].index[0]
+        gg = gg[(gg.index <= v1) & (gg.index >= v2)]
+        gg = gg.reset_index(drop=True)
+        # gg["Condition"] = "Orginal"
+        # gg = FillGaps(gg)
+
+    #####
+
+    gg["percent_2"] = (gg["Number"] / gg["Total"] * 100).round(2)
+    gg.loc[abs(gg.Percent - gg.percent_2) > 0.010000001, "Percent"] = np.nan
+    gg.loc[gg.Percent.isnull(), "Total"] = np.nan
+    gg["Percent"] = gg.Percent.fillna(method="bfill").fillna(method="ffill")
+    gg["Total"] = gg.Total.fillna(method="bfill").fillna(method="ffill")
+
+    gg = gg.drop(columns=["percent_2"])
+
+    return gg
+
+
+def FlatFunction(gg):
+
+    cg = gg[(gg.Percent_Change != 0) & (~gg.Percent_Change.isnull())]
+
+    cindex = cg.index
+    gindex = gg.index
+
+    for index in cindex:
+        i = 1
+        flatted = 0
+        while i < 6 and flatted == 0:
+            ids = list(range(index, index + i))
+            dg = gg[gg.index.isin(ids)]
+            i += 1
+            if abs(dg.Percent_Change.sum()) <= 0.00001:
+                pg = gg.loc[gg.index == index - 1]
+                if len(pg) < 1:
+                    continue
+                dgindex = dg.index
+                for dinex in dgindex:
+                    gg.loc[gg.index == dinex, "Number"] = pg["Number"].iloc[0]
+                    gg.loc[gg.index == dinex, "Percent"] = pg["Percent"].iloc[0]
+                    gg.loc[gg.index == dinex, "Percent_Change"] = 0
+                    gg.loc[gg.index == dinex, "Condition"] = "Flatted"
+
+                flatted = 1
+                a = set(cindex)
+                b = set(ids)
+                cindex = list(a.difference(b))
+    return gg
+
+
+def FillGaps(gg):
+    ChangeList = [
+        "name",
+        "stock_id",
+        "group_name",
+        "group_id",
+        "Holder",
+        "Number",
+        "type",
+        "Percent",
+        "Total",
+        "close_price",
+    ]
+    nanid = list(gg[gg.name.isnull()].index)
+    for value in nanid:
+        NextValue = gg[(~gg.name.isnull()) & (gg.index > value)].index[0]
+        if NextValue - value <= 4:
+            for i in ChangeList:
+                gg.loc[gg.index == value, i] = gg.loc[gg.index == NextValue][i].iloc[0]
+        gg.loc[gg.index == value, "Condition"] = "Filled"
+    gg = gg[~(gg.name.isnull())]
+    return gg
+
+
+df = df1.reset_index(drop=True)
+import requests
+
+
+def removeSlash(row):
+    X = row.split("/")
+    if len(X[1]) < 2:
+        X[1] = "0" + X[1]
+    if len(X[2]) < 2:
+        X[2] = "0" + X[2]
+
+    return int(X[0] + X[1] + X[2])
+
+
+def Overall_index():
+    url = (
+        r"http://www.tsetmc.com/tsev2/chart/data/Index.aspx?i=32097828799138957&t=value"
+    )
+    r = requests.get(url)
+    jalaliDate = []
+    Value = []
+    for i in r.text.split(";"):
+        x = i.split(",")
+        jalaliDate.append(x[0])
+        Value.append(float(x[1]))
+    df = pd.DataFrame(
+        {
+            "jalaliDate": jalaliDate,
+            "Value": Value,
+        },
+        columns=["jalaliDate", "Value"],
+    )
+    df["jalaliDate"] = df.jalaliDate.apply(removeSlash)
+    return df
+
+
+grouped_data = df1.groupby(["name", "Holder_id"])  # ,'type'])
+g_keys = list(grouped_data.groups.keys())
+
+data = pd.DataFrame()
+print(len((g_keys)))
+#%%
+i = g_keys[3195]
+i = ("کفرا", 99.0)
+gg = Cleaning(grouped_data.get_group(i))
+gg[gg.date > 20081229.0]
+# %%
+from tqdm import tqdm
+
+tqdm.pandas()
+data = grouped_data.progress_apply(Cleaning)
+data = (
+    (data.reset_index(drop=True).dropna())
+    .sort_values(by=["name", "date", "Percent"])
+    .reset_index(drop=True)
+)
+#%%
+data[data.name == 'کفرا'][data.date == 20081224]
+
+
+
+#%%
+data['percentSum'] = data.groupby(['name','date']).Percent.transform(sum)
+data.loc[data.percentSum > 100, "Percent"] = np.nan
+data.loc[data.Percent.isnull(), "Total"] = np.nan
+data["Percent"] = data.groupby(["name", "Holder_id"]).Percent.fillna(method="bfill").fillna(method="ffill")
+data["Total"] = data.groupby(["name", "Holder_id"]).Total.fillna(method="bfill").fillna(method="ffill")
+
+####
+
+data['percentSum'] = data.groupby(['name','date']).Percent.transform(sum)
+data.loc[data.percentSum > 100, "Percent"] = np.nan
+data.loc[data.Percent.isnull(), "Total"] = np.nan
+data["Percent"] = data.groupby(["name", "Holder_id"]).Percent.fillna(method="ffill").fillna(method="bfill")
+data["Total"] = data.groupby(["name", "Holder_id"]).Total.fillna(method="ffill").fillna(method="bfill")
+data = data.drop(columns = ['percentSum'])
+
+
+
+#%%
+def findLow(data, dif):
+    a = data.groupby("date").apply(lambda x: len(x)).to_frame().reset_index()
+    a.plot(y=0, use_index=True)
+    a["moving_average"] = a[0].rolling(10, min_periods=1).mean()
+    a["change"] = a.moving_average - a[0]
+    tempt = (a.loc[a[0] < a.moving_average - dif]).sort_values(by="date")
+    return a, tempt
+
+
+####
+def clean(tempt, data, a):
+    result = pd.DataFrame()
+    for i in tqdm(tempt.date):
+        pdate = a[a.date < i].tail(1).date.iloc[0]
+        new = pd.concat([pd.DataFrame(), data.loc[data.date == pdate]])
+        new["date"] = i
+        data = data.loc[data.date != i]
+        result = pd.concat([result, new])
+    data = pd.concat([data, new]).sort_values(by=["name", "date", "Percent"])
+    data.groupby("date").apply(lambda x: len(x)).to_frame().reset_index().plot(
+        y=0, use_index=True
+    )
+    return data
+
+
+#%%
+a, tempt = findLow(data, 50)
+data = clean(tempt, data, a)
+a, tempt = findLow(data, 25)
+data = clean(tempt, data, a)
+#%%
+tempt = pdf[["jalaliDate", "date"]].drop_duplicates()
+mapingdict = dict(zip(tempt.date, tempt.jalaliDate))
+data["jalaliDate"] = data.date.map(mapingdict)
+# %%
+data[data.name == 'البرز'][data.date == 20110713]
 #%%
